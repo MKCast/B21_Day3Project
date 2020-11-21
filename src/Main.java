b0c768192a8803caa5828e7e87061cfd6a559a7c
mayitokarolinacastro <mayitokarolina.castro@gmail.com>
2020-11-21 06:31:36 -0700

    Nice things about B21

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello Git Last Day");
+        //B21 is excited
+        //B21 is dedicated
+        //B2121 is sucsccessful tester
     }
 }
